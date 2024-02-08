d864755509339e95f9367cb2191d4496897de6d1
wasd <wasd@wasd.xyz>
2024-02-08 15:56:30 +0100

    added postgres conn to appsettings

--- TodoApi/db_config/KundeDb.cs
+++ TodoApi/db_config/KundeDb.cs
@@ -1,15 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 
+
 public class KundeDb : DbContext
 {
-    // Add DbSet properties for your entities
     public DbSet<Kunde> kunden { get; set; }
-    
-
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        // Configure your local PostgreSQL connection string here
+    {   //TODO serv config aus app json holen
         optionsBuilder.UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Password=admin");
     }
 }
--- TodoApi/kunde/KundeApi.cs
+++ TodoApi/kunde/KundeApi.cs
@@ -1,8 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add DbContext to the service collection
 builder.Services.AddDbContext<KundeDb>();
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
+// Enable CORS
+app.UseCors();
 
 const string baseApi = "api/kunde";
 var kunden = app.MapGroup(baseApi);
@@ -22,19 +39,19 @@ static async Task<IResult> CreateKunde(Kunde kunde, KundeDb db)
 {
     db.Add(kunde);
     await db.SaveChangesAsync();
-    
+
     return TypedResults.Created($"{baseApi}/{kunde.Id}", kunde);
 }
 
 static async Task<IResult> UpdateKunde(int id, Kunde inputKunde, KundeDb db)
 {
     var kunde = await db.kunden.FindAsync(id);
+
     if (kunde is null) return TypedResults.NotFound();
     kunde.tel = inputKunde.tel;
     kunde.adresse = inputKunde.adresse;
     kunde.vorname = inputKunde.vorname;
     kunde.nachname = inputKunde.nachname;
-
     await db.SaveChangesAsync();
     return TypedResults.NoContent();
 }
@@ -50,5 +67,4 @@ static async Task<IResult> DeleteKunde(int id, KundeDb db)
 
     return TypedResults.NotFound();
 }
-
 app.Run();
