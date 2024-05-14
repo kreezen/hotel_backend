7091b8c1d5f8e2278de5608ac00bb240a3604c80
wasd <wasd@wasd.xyz>
2024-05-14 17:58:01 +0200

    changed file structure

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3e6c1c42724ffc399c5ceb5908df2afec04cf7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cee6ac2c099fa750eead28981ab4c254e52ed2bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
